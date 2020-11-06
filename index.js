3b56cda821beaf51a0b786e4f67c1248501e4e5b
rohith0696 <60017598+rohith0696@users.noreply.github.com>
2020-11-06 17:59:18 -0600

    added dotenv and added database

--- index.js
+++ index.js
@@ -4,7 +4,16 @@ const app = express();
 const engines = require('consolidate')
 const path = require('path')
 const port = process.env.PORT || 3000;
+const dotenv = require('dotenv')
+const mongoose = require('mongoose')
 // process.title = myApp;
+
+
+dotenv.config({ path: '.env' })
+console.log('Environment variables loaded into process.env.')
+
+const isProduction = process.env.NODE_ENV === 'production'
+console.log(`Environment isProduction = ${isProduction}`)
 
 //set the root view folder
 app.set('views', path.join(__dirname, './views'))
@@ -15,6 +24,34 @@ app.engine('ejs', engines.ejs)
 
 const router = require('./routes/router')
 app.use('/', router)
+
+
+// Connect to NoSQL datastore........................
+
+// choose the connection
+const dbURI = isProduction ? encodeURI(process.env.ATLAS_URI) : encodeURI(process.env.LOCAL_MONGODB_URI)
+console.log('MongoDB URL = ' + dbURI)
+
+// get dbName
+const DB_NAME = process.env.DB_NAME
+
+// set connection options
+const connectionOptions = {
+  dbName: DB_NAME,
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true
+}
+
+// use mongoose to connect & create a default connection
+mongoose.connect(dbURI, connectionOptions, (err, client) => {
+  if (err) { LOG.error('Error in MongoDB connection : ' + JSON.stringify(err, undefined, 2)) }
+  console.log('MongoDB connection succeeded.')
+})
+
+// Get the default connection
+const connection = mongoose.connection
+
 
 app.listen(port,()=>
   {
