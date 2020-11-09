692f546a46201957ae0eab4fc6001ae9c32ddae5
rohith0696 <60017598+rohith0696@users.noreply.github.com>
2020-11-09 14:32:34 -0600

    Added routes to the views

--- routes/router.js
+++ routes/router.js
@@ -7,4 +7,64 @@ router.get('/',(req,res)=>
     res.render('./user/loginpage',{ title: 'Login', layout: false })
 })
 
+router.get('/index', (req, res) => {
+    res.render('./user/loginpage', { title: 'Login', layout: false})
+  })
+
+  router.get('/Home', (req, res, next) => {
+    res.render('./partials/index', { title: 'The Hunt' })
+  })
+  
+  router.get('/user/Login', (req, res, next) => {
+    res.render('./user/loginpage', { title: 'Login', layout: false})
+  })
+  
+  router.get('/user/Register', (req, res, next) => {
+    res.render('./user/signuppage', { title: 'Register', layout: false})
+  })
+  
+  router.get('/user/forgotpassword', (req, res, next) => {
+    res.render('./user/forgotpassword', { title: 'Forgot password', layout: false})
+  })
+  
+  router.get('/user/forgotemail', (req, res, next) => {
+    res.render('./user/forgotemail', { title: 'Fotgot email', layout: false})
+  })
+  
+  router.get('/user/newPassword', (req, res, next) => {
+    res.render('./user/newpassword', { title: 'New Password', layout: false})
+  })
+  
+  router.get('/404' , (req, res, next) => {
+    res.render('./partials/error', { title: 'Error', layout: false})
+  })
+  
+  router.get('/create_competition', (req, res, next) => {
+    res.render('./competition/create', { title: 'Register'})
+  })
+  
+  router.get('/createScreen', (req, res, next) => {
+    res.render('createScreen', { title: 'Create Screen'})
+  })
+  
+  router.get('/quest/createQuest', (req, res, next) => {
+    res.render('./quest/create', { title: "Quest"})
+  })
+  
+  router.get('/user/profile', (req, res, next) => {
+    res.render('./player/details', { title: "profile"})
+  })
+  
+  
+  router.get('/team/createTeam', (req, res, next) => {
+    res.render('./team/create', { title: "Team"})
+  })
+  
+  
+  router.get('/team/invitePlayers', (req, res, next) => {
+    res.render('./team/invitePlayers', { title: "Invite Players"})
+  }) 
+  router.get('/location/delete', (req, res, next) => {
+    res.render('./location/delete', { title: "Locationdelete"})
+  })
 module.exports = router;
