512419511351da259969482bd44347445d597fb0
rohith0696 <60017598+rohith0696@users.noreply.github.com>
2020-11-13 07:50:50 -0600

    updated signup

--- controllers/user/userController.js
+++ controllers/user/userController.js
@@ -1,16 +1,33 @@
 const express = require('express')
 const api = express.Router()
+const bcrypt = require('bcrypt')
 
 const bodyParser = require('body-parser');
 api.use(bodyParser.json())
 api.use(bodyParser.urlencoded({ extended: true }));
 const Model = require('../../models/user')
 
-api.post('/Register',(req,res)=>{
+api.post('/try', (req,res) => {
+    console.log("login here")
+   Model.findOne({"email": req.body.email}).then(user => {
+     if(!user) res.status(404).json({'err': "user not there"})
+     else{
+       bcrypt.compare(req.body.password, user.password, (error, match) => {
+         if(error) res.status(500).json(error)
+         if(match) res.status(200).json({"user": user})
+       })
+     }
+   })
+   .catch(err => {
+     console.log(err,"error is here")
+   })
+ })
+
+api.post('/signup',async(req,res)=>{
     const body = req.body
     const user = new Model(body)
     console.log(user,"body is here")
-    user.save((err) => {
+    await user.save((err) => {
         if(err){
             return res.status().json({"msg": err})
         }else{
@@ -19,7 +36,7 @@ api.post('/Register',(req,res)=>{
               data: user
           })
         }
-      
+      9
     })
 })
 
